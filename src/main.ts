7f5cab1c6919b29d33da269f022ab21379be4206
victormedeiros1 <josevictordev@gmail.com>
2024-05-25 01:54:39 -0300

    feat: ESLint, Prettier, SASS, Styles and HELLO WORLD

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import './styles/_global.scss'
+
+const app = createApp(App)
+
+app.mount('#app')
