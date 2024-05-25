5487a57333a8d9333a494f9f749fa63d9bfd2aa5
victormedeiros1 <josevictordev@gmail.com>
2024-05-25 12:10:16 -0300

    feat: form to create habits

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,85 @@
 <template>
-  <h1>Hello World, Habits is my new app!</h1>
+  <div class="container">
+    <div class="habbits">
+      <h1 class="habbits__title">Habbits</h1>
+
+      <form class="form" @submit="createNewHabit">
+        <div class="form__group">
+          <label id="form-label" class="form__label" for="form-input"
+            >Qual o novo hábito?
+          </label>
+          <input
+            id="form-input"
+            class="form__input"
+            type="text"
+            v-model="name"
+          />
+        </div>
+
+        <div class="form__footer">
+          <button class="form__button">Adicionar</button>
+        </div>
+      </form>
+
+      <div class="habbits__habits">
+        <div
+          v-for="(habit, index) in habits"
+          :key="habit.id"
+          class="habbits__habit"
+          :class="`habbits__habit-${index}`"
+        >
+          <input
+            :id="`habit-input-${index}`"
+            type="checkbox"
+            v-model="habit.done"
+          />
+          <label :id="`habit-label-${index}`" :for="`habit-input-${index}`">{{
+            habit.name
+          }}</label>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { v4 as uuidv4 } from 'uuid'
+  import { ref } from 'vue'
 
-<style scoped lang="scss"></style>
+  interface Habit {
+    id: string
+    name: string
+    done: boolean
+  }
+
+  const name = ref<string>('')
+  const habits = ref<Habit[]>([])
+
+  function createNewHabit(event: Event): void {
+    event.preventDefault()
+
+    habits.value.push({
+      id: uuidv4(),
+      name: name.value,
+      done: false
+    })
+  }
+</script>
+
+<style scoped lang="scss">
+  .container {
+    padding: 0 $p-20;
+  }
+
+  .form {
+    display: flex;
+    flex-direction: column;
+    gap: $g-16;
+
+    &__group {
+      display: flex;
+      flex-direction: column;
+      gap: $g-8;
+    }
+  }
+</style>
